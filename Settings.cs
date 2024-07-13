5be7b2c4249fafdc3ea602b194f4f5af3d3814ae
SomeoneSom <52360990+SomeoneSom@users.noreply.github.com>
2024-07-13 09:49:41 -0400

    start on settings rework

---
+++ Settings.cs
@@ -0,0 +1,29 @@
+namespace Featherline;
+public class Settings
+{
+    public static string? Favorite; 
+    public static int Framecount = 120;
+
+    public static int Population = 50;
+    public static int Generations = 2000;
+    public static int SurvivorCount = 20;
+
+    public static float MutationMagnitude = 8;
+    public static int MaxMutChangeCount = 5;
+
+    public static string[]? Checkpoints;
+
+    public string? CustomSpinnerNames;
+
+    public static bool AvoidWalls = false;
+
+    public static string[]? ManualHitboxes;
+
+    public static bool FrameBasedOnly = false;
+    public static bool TimingTestFavDirectly = false;
+    public static int GensPerTiming = 150;
+
+    public static int ShuffleCount = 6;
+
+    public static int MaxThreadCount = Environment.ProcessorCount;
+}
